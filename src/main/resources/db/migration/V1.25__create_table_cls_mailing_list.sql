e845e6ef8c6bf460eeea38a2ffc0e9f0d5ace762
liudmila <gonchikova94@gmail.com>
2020-11-17 16:57:25 +0800

    #815 Автоматически создавать Системную рассылку

--- src/main/resources/db/migration/V1.25__create_table_cls_mailing_list.sql
+++ src/main/resources/db/migration/V1.25__create_table_cls_mailing_list.sql
@@ -7,3 +7,7 @@ create table if not exists cls_mailing_list
     description     text,
     status          smallint
 );
+
+INSERT INTO cls_mailing_list(id, name) VALUES (1, 'Системная рассылка') ON CONFLICT
+    ON CONSTRAINT cls_mailing_list_pkey DO UPDATE SET name = 'Системная рассылка';
+
