3ad21e6264e840b3b9ab83a387af3ea878104e42
alxtmf <89021632777@mail.ru>
2020-12-02 13:48:26 +0800

    #833 таблица для хранения файлов организаций

---
+++ src/main/resources/db/migration/V1.31__create_table_cls_organization_contact.sql
@@ -0,0 +1,18 @@
+
+create table if not exists reg_organization_file
+(
+    id serial not null
+        constraint reg_organization_file_pkey primary key,
+    id_organization integer not null
+        constraint fk_cls_organization references cls_organization,
+    id_request integer  --ссылка на заявку (для ранее загруженных файлов, либо если файл загружается при подаче заявки)
+        constraint fk_doc_request references doc_request,
+    is_deleted boolean, -- пометка мягкого удаления
+    time_create timestamp not null default current_timestamp, -- датавремя создания
+    attachment_path text, -- путь к файлу относительно каталога приложения + имя файла
+    file_name text, --имя файла
+    original_file_name text, -- имя файла при загрузке
+    file_extension varchar(16), --расширение файла
+    hash text, -- хэшсумма файла
+    file_size decimal(10,2) -- размер файла
+);
