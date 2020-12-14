7c5520ee0c1cd02e76f25dc15df6dcc38fcaa131
alxtmf <89021632777@mail.ru>
2020-12-14 18:42:25 +0800

    reg_help inserts

--- src/main/resources/db/migration/V1.48__create_table_reg_help.sql
+++ src/main/resources/db/migration/V1.48__create_table_reg_help.sql
@@ -9,17 +9,24 @@ create table if not exists reg_help
     time_create timestamp not null default current_timestamp
 );
 
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (1, 'Departments', 'Подразделения', 'Departments', '2020-12-14 09:25:48.067721');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (2, 'DepartmentUsers', 'Пользователи подразделений', 'DepartmentUsers', '2020-12-14 09:25:48.067721');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (3, 'Organizations', 'Организации', 'Organizations', '2020-12-14 09:25:48.067721');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (4, 'Requests', 'Заявки', 'Requests', '2020-12-14 09:25:48.067721');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (5, 'TypeRequests', 'Предписания', 'TypeRequests', '2020-12-14 09:25:48.067721');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (6, 'RestrictionTypes', 'Типы ограничений', 'RestrictionTypes', '2020-12-14 01:37:00.327321');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (7, 'Principals', 'Пользователи', 'Principals', '2020-12-14 01:37:00.327321');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (8, 'Templates', 'Шаблоны сообщений', 'Templates', '2020-12-14 01:37:00.327321');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (9, 'Statistic', 'Статистика', 'Statistic', '2020-12-14 01:37:00.327321');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (10, 'Okveds', 'ОКВЭДы', 'Okveds', '2020-12-14 01:37:00.327321');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (11, 'Mailing', 'Типы рассылок', 'Mailing', '2020-12-14 01:37:00.327321');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (12, 'MailingMessages', 'Сообщения рассылок', 'MailingMessages', '2020-12-14 01:37:00.327321');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (13, 'Fias', 'Загрузка ФИАС ЕГРЮЛ', ' ЕГРЮЛ', '2020-12-14 01:37:00.327321');
-INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (14, 'News', 'Новости', 'News', '2020-12-14 01:37:00.327321');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (6, 'TypeRequests', 'Предписания', 'Здесь отображаются информация по предписаниям', '2020-12-14 09:25:48.067721');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (15, 'News', 'Новости', 'Здесь отображаются новости', '2020-12-14 01:37:00.327321');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (9, 'Templates', 'Шаблоны сообщений', 'Шаблоны сообщений', '2020-12-14 01:37:00.327321');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (11, 'Okveds', 'ОКВЭДы', 'Здесь отображаются ОКВЭДы (ОКВЭД - Общероссийский классификатор видов экономической деятельности)', '2020-12-14 01:37:00.327321');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (10, 'Statistic', 'Статистика', 'Статистика', '2020-12-14 01:37:00.327321');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (5, 'Requests', 'Заявки', 'Здесь отображаются информация по заявкам', '2020-12-14 09:25:48.067721');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (24, 'Prescript', 'Предписания', 'Здесь отображаются информация по предписаниям', '2020-12-14 05:54:00.089726');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (19, 'CommonInfo', 'Общая информация', 'Здесь отображается информация об организации', '2020-12-14 10:16:35.472504');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (21, 'Settings', 'Настройки', 'Настройки', '2020-12-14 05:54:00.089726');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (23, 'Address', 'Фактические адреса', 'Здесь собраны фактические адреса организации', '2020-12-14 05:54:00.089726');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (22, 'Documents', 'Документы', 'Здесь отображается информация по документам', '2020-12-14 05:54:00.089726');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (3, 'DepartmentUsers', 'Пользователи подразделений', 'Здесь отображается информация по пользователям подразделениям', '2020-12-14 09:25:48.067721');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (13, 'MailingMessages', 'Сообщения рассылок', 'Здесь отображаются сообщения рассылок', '2020-12-14 01:37:00.327321');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (14, 'Fias', 'Загрузка ФИАС ЕГРЮЛ', 'Федеральная информационная адресная система<br>Единый государственный реестр юридических лиц', '2020-12-14 01:37:00.327321');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (25, 'Contacts', 'Контакты', 'Здесь отображаются контактные номера ', '2020-12-14 05:54:00.089726');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (20, 'Employees', 'Сотрудники', 'Здесь отображается информация по сотрудникам', '2020-12-14 05:54:00.089726');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (12, 'Mailing', 'Типы рассылок', 'Здесь отображаются типы рассылок', '2020-12-14 01:37:00.327321');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (2, 'Departments', 'Подразделения', 'Здесь отображается информация по подразделениям ', '2020-12-14 09:25:48.067721');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (7, 'RestrictionTypes', 'Типы ограничений', 'Здесь отображаются информация по типам ограничений', '2020-12-14 01:37:00.327321');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (4, 'Organizations', 'Организации', 'Здесь отображаются информация по организациям', '2020-12-14 09:25:48.067721');
+INSERT INTO public.reg_help (id, key, name, description, time_create) VALUES (8, 'Principals', 'Пользователи', 'Здесь отображаются информация по пользователям', '2020-12-14 01:37:00.327321');
