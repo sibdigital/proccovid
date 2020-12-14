4ab822262816fa4ae934644072b99f73ac5d2f41
alxtmf <89021632777@mail.ru>
2020-12-14 18:50:49 +0800

    изменение стилей расцветки ячеек

--- src/main/resources/db/migration/V1.45__request_status_cls_setting.sql
+++ src/main/resources/db/migration/V1.45__request_status_cls_setting.sql
@@ -1,3 +1,6 @@
-insert into cls_settings(status, key, value, string_value) values (1, 'requestStatusReview0', '{"background-color": "#ffe9a7"}', 'Заявка на рассмотрении.');
-insert into cls_settings(status, key, value, string_value) values (1, 'requestStatusReview1', '{"background-color": "#baffa7"}', 'Заявка одобрена.');
-insert into cls_settings(status, key, value, string_value) values (1, 'requestStatusReview2', '{"background-color": "#ffa7a7"}', 'Заявка отклонена.');
+insert into cls_settings(status, key, value, string_value)
+values (1, 'requestsStyles', '{
+        "oncheck": {"background-color": "#ffe9a7"},
+        "cancelled": {"background-color": "#ffa7a7"},
+        "confirmed": {"background-color": "#baffa7"}
+}','Стили заявок');
