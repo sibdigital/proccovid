3d51eb4feb986dbe594ce41300ffd0d5faca3037
liudmilagy <gonchikova94@gmail.com>
2021-04-16 17:09:55 +0800

    tree fix

--- src/main/resources/static/js/admin/reports/inspectionReport.js
+++ src/main/resources/static/js/admin/reports/inspectionReport.js
@@ -1,100 +1,3 @@
-let mainOkvedTree = {
-    view: "tree",
-    id: "mainOkvedTreeId",
-    template: "{common.icon()} {common.checkbox()} {common.folder()} <span>#value#</span>",
-    threeState: true,
-    data: [
-        {
-            id: 'root',
-            value: 'ОКВЭДЫ',
-            // open: true,
-            data: [
-                {
-                    id: '2014',
-                    value: '2014',
-                    webix_kids: true
-                },
-                {
-                    id: '2001',
-                    value: '2001',
-                    webix_kids: true
-                }
-            ]
-        }
-    ],
-    on: {
-        onDataRequest: (parentNode) => {
-            $$('mainOkvedTreeId').parse(
-                webix.ajax().get(
-                    'okved_tree', {parent_node: parentNode}
-                ).then((data) => {
-                    data = {
-                        parent: parentNode,
-                        data: data.json().map((e) => {
-                            return {
-                                id: e.id,
-                                value: e.value,
-                                webix_kids: true
-                            }
-                        })
-                    };
-                    return data;
-                })
-            );
-            return false;
-        }
-    }
-}
-
-let additionalOkvedTree = {
-    view: "tree",
-    id: "additionalOkvedTreeId",
-    template: "{common.icon()} {common.checkbox()} {common.folder()} <span>#value#</span>",
-    threeState: true,
-    data: [
-        {
-            id: 'root',
-            value: 'ОКВЭДЫ',
-            // open: true,
-            data: [
-                {
-                    id: '2014',
-                    value: '2014',
-                    webix_kids: true
-                },
-                {
-                    id: '2001',
-                    value: '2001',
-                    webix_kids: true
-                }
-            ]
-        }
-    ],
-    on: {
-        onDataRequest: (parentNode) => {
-            $$('additionalOkvedTreeId').parse(
-                webix.ajax().get(
-                    'okved_tree', {parent_node: parentNode}
-                ).then((data) => {
-                    data = {
-                        parent: parentNode,
-                        data: data.json().map((e) => {
-                            return {
-                                id: e.id,
-                                value: e.value,
-                                webix_kids: true
-                            }
-                        })
-                    };
-                    return data;
-                })
-            );
-            return false;
-        }
-    }
-}
-
-
 const inspectionReport = {
     body: {
         type: "space",
@@ -184,7 +87,8 @@ const inspectionReport = {
                         collapsed:true,
                         body: {
                             rows: [
-                                mainOkvedTree,
+                                tree('mainOkvedTreeId'),
+                                // mainOkvedTree,
                             ]
                         }
                     }
@@ -199,7 +103,8 @@ const inspectionReport = {
                         collapsed:true,
                         body: {
                             rows: [
-                                additionalOkvedTree,
+                                tree('additionalOkvedTreeId'),
+                                // additionalOkvedTree,
                             ]
                         }
                     }
