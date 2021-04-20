20a68c24c89124b2603c9a083a0768316ac65460
eugene <poebot3r@gmail.com>
2021-04-20 14:27:03 +0800

    Create V1.93__change_inn_org_of_reg_violation_search.sql

---
+++ src/main/resources/db/migration/V1.93__change_inn_org_of_reg_violation_search.sql
@@ -0,0 +1,4 @@
+alter table reg_violation_search alter column inn_org type text;
+alter table reg_violation_search alter column number_file type text;
+alter table reg_person_violation_search alter column fio type text;
+alter table reg_person_violation_search alter column number_file type text;
