a0c84e22188a9a5dda29b0ce4de1e2b38c583f8b
shedoev <shedoev@gmail.com>
2021-04-22 15:21:52 +0800

    #813 Скрипт добавления настроек для ЕСИА

---
+++ src/main/resources/db/migration/V1.95__add_settings_for_esia.sql
@@ -0,0 +1,9 @@
+insert into cls_settings(status, key, string_value) values (1,'esia.enable', 'false');
+insert into cls_settings(status, key, string_value) values (1,'esia.client-id', 'COVID2019RB');
+insert into cls_settings(status, key, string_value) values (1,'esia.authorization.url', 'https://esia-portal1.test.gosuslugi.ru/aas/oauth2/ac');
+insert into cls_settings(status, key, string_value) values (1,'esia.token.url', 'https://esia-portal1.test.gosuslugi.ru/aas/oauth2/te');
+insert into cls_settings(status, key, string_value) values (1,'esia.api.url', 'https://esia-portal1.test.gosuslugi.ru/rs');
+insert into cls_settings(status, key, string_value) values (1,'esia.user.scopes', 'fullname inn email self_employed usr_org');
+insert into cls_settings(status, key, string_value) values (1,'esia.organization.scopes' ,'org_shortname org_fullname org_inn org_ogrn org_emps');
+insert into cls_settings(status, key, string_value) values (1,'esia.keystore.password', '123456');
+insert into cls_settings(status, key, string_value) values (1,'esia.keystore.alias', 'ХамархановаИК 2021-02-05 10-31-59');
