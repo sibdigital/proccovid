5375be79111ea8d50a82647bbd08ddf6467e0985
liudmila <gonchikova94@gmail.com>
2021-07-12 14:39:03 +0800

    insert noRightToApplyRequestSubsidy to cls_settings

---
+++ src/main/resources/db/migration/V1.110__insert_noRightToApplyRequestSubsidy.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.cls_settings (status, key, value, string_value) VALUES (1, 'noRightToApplyRequestSubsidy', null, '<p style="text-align: center;"><strong>У вас отсутствуют виды деятельности по классификатору ОКВЭД, дающие право на меры поддержки</strong></p>');
