39f84b7385ceaf0ce60687e4dc7ed3a8572d1034
alxtmf <89021632777@mail.ru>
2021-07-18 20:33:00 +0800

    Доработки

--- src/main/resources/db/migration/V1.121__add_weight_in_required_diles_subsidy.sql
+++ src/main/resources/db/migration/V1.121__add_weight_in_required_diles_subsidy.sql
@@ -2,3 +2,8 @@ alter table subs.tp_required_subsidy_file
     add column if not exists weight integer default 0; -- порядок сортировки
 alter table okved
     add column if not exists id_parent uuid; --добавим родителя в ОКВЭД
+
+INSERT INTO public.cls_settings (status, key, value, string_value)
+VALUES
+(1, 'enableExpirementalFeatures', null, 'false')
+;
