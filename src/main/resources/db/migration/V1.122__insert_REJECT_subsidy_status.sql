ba8499c3611bd5ca686db61e70ffa508494db1a2
alxtmf <89021632777@mail.ru>
2021-07-20 09:00:24 +0800

    add reject status

---
+++ src/main/resources/db/migration/V1.122__insert_REJECT_subsidy_status.sql
@@ -0,0 +1,2 @@
+INSERT INTO subs.cls_subsidy_request_status (id_subsidy, is_deleted, name, short_name, code, is_block_request)
+ VALUES (null, false, 'Отклоненная заявка', 'Отклонена', 'REJECT', true);
